1669f6be617fc49c4323020f2beed3c992248d49
ryuji9896 <makinoryujijoker9896@i.softbank.jp>
2023-12-15 13:35:58 +0000

    #で回答番号を追加

--- practice.php
+++ practice.php
@@ -1,21 +1,25 @@
 <?php
+#1:回答
 $a = 3 ;
 $b = 7 ;
 echo $a + $b;
 
-
+#2:回答
 $array_month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
 echo $array_month[7];
 
+#3:回答
 $hello = "Hello, ";
 $name = "Ryuji";
 $world = "'s World!";
 echo $hello . $name . $world;
 
-$tech_boost = 'tech';
-$tech_boost .= 'boost';
+#4:回答
+$tech_boost = "tech";
+$tech_boost .= "boost";
 echo $tech_boost;
 
+#5:回答
 $_2018_calender = [
     "january" => "1月",
     "february" => "2月",
